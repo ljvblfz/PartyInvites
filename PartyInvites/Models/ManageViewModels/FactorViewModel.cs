a010b6e729b2279e81b6b6dfa1a5f4575a01a54c
ljvblfz <ljvblfz@users.noreply.github.com>
2017-02-06 17:10:20 +0800

    Add source code files created by Rider IDE.

---
+++ PartyInvites/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,7 @@
+namespace PartyInvites.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
---
+++ PartyInvites/Models/ManageViewModels/RemoveLoginViewModel.cs
@@ -0,0 +1,8 @@
+namespace PartyInvites.Models.ManageViewModels
+{
+    public class RemoveLoginViewModel
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+    }
+}
